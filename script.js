fb1a32cd5b88a68a7e51169d6da09491a9553f22
Zahid <zahiiahmad786@gmail.com>
2026-03-27 00:28:16 +0530

    chnaged

--- script.js
+++ script.js
@@ -63,6 +63,4 @@ form.reset();
 
 });
 
-/* ================= HAMBURGER MENU ================= */ 
-const toggle = document.getElementById("menu-toggle"); const nav = document.getElementById("nav-menu"); 
-toggle.addEventListener("click", () => { nav.classList.toggle("active"); });
+document.addEventListener("DOMContentLoaded", function(){ const toggle = document.getElementById("menu-toggle"); const nav = document.getElementById("nav-menu"); toggle.addEventListener("click", function(){ nav.classList.toggle("active"); }); });
